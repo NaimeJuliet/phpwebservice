51c6f14223eb09a98743a11f57dfc48460fe00fe
505 <505@academicos.cesde.edu.co>
2022-05-17 15:31:29 -0500

    se guarda pho de webservice

---
+++ consultarVenta.php
@@ -0,0 +1,22 @@
+<?php
+
+    if(isset($_REQUEST['codventa'])){
+
+        $codventa = $_REQUEST['codventa'];
+
+
+        $cnx = mysqli_connect("localhost","root","","bdusuarios")or die("No se pudo conectar con la base de datos");
+        $res = $cnx->query("SELECT * FROM ventas WHERE codventa = '$codventa'");
+
+        $json = array();
+
+        foreach($res as $row) {
+            $json['datos'][] = $row;
+        }
+
+        echo json_encode($json);
+
+    }else{
+        echo "Se requiere un codigo de venta para consultar";
+    }
+?>
